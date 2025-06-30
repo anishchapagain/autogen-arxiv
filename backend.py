bce9f79caec546ff9bed0535e6a7479419c8a96a
Anish Chapagain <anishchapagain@gmail.com>
2025-06-30 21:01:49 +0545

    adding arxiv

--- backend.py
+++ backend.py
@@ -1,19 +1,64 @@
 from autogen_agentchat.agents import AssistantAgent
-from autogen_ext.models.ollama import OllamaModel
-from autogen_ext.models.openai import OpenAIModel
+from autogen_ext.models.ollama import OllamaChatCompletionClient 
+# from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_agentchat.teams import AgentTeam
 
 from dotenv import load_dotenv
 
 import os
+import arxiv
+from typing import List, Dict, AsyncGenerator
 
 load_dotenv() # Load environment variables from .env file
 
-system_message = (
-    "You are a helpful assistant that summarizes academic papers from arXiv."
-    "You will be provided with the title, abstract, and other relevant information of a paper.\n"
-    "Your task is to generate a concise summary that captures the main contributions and findings of the paper."
-    "Please ensure that your summary is clear, informative, and suitable for readers who may not be familiar with the specific field of study.\n"
-    "Avoid jargon and focus on the key points that would help someone understand the significance of the research."
+# Librariies
+# pip install -U "autogen-ext[ollama]"
+# pip install -U "autogen-ext[openai]"
+
+def arxiv_search(query: str, max_results: int = 10) -> List[Dict]:
+    """
+    Search for papers on arXiv using the provided query.
+    https://pypi.org/project/arxiv/
+
+    Args:
+        query (str): The search query.
+        max_results (int): Maximum number of results to return.
+        
+    Returns:
+        List[Dict]: A list of dictionaries containing paper information.
+    """
+    # Construct the default API client.
+    client = arxiv.Client()
+
+    search = arxiv.Search(
+        query=query,
+        max_results=max_results,
+        sort_by=arxiv.SortCriterion.Relevance # arxiv.SortCriterion.SubmittedDate
+    )
+    
+    # Data Collection
+    arxiv_results:List[Dict] = []
+
+    # Perform the search and collect results
+    results = client.results(search)
+    for result in results:
+        arxiv_results.append({
+            "title": result.title,
+            "authors": [author.name for author in result.authors],
+            "summary": result.summary,
+            "published": result.published.isoformat(), # strftime("%Y-%m-%dT%H:%M:%S"),
+            "link": result.entry_id,
+            "url": result.pdf_url,
+            "categories": result.categories,
+        })
+    
+    return arxiv_results
+
+# Define the system message for the summarizer agent
+system_message_arxiv = (
+    "Given a user topic, think of the best arxiv query and call the provided tool."
+    "Always getch five times the papers related to the topic."
+    "When the tool returns the results, summarize each paper in a concise JSON manner."
 )
 
 system_message =(
@@ -27,25 +72,29 @@ system_message =(
     "4. Use clear and concise language, avoiding jargon\n" \
     )
 
-agent_LLM_OpenAI = OpenAIModel( # On OpenAI API
-    api_key = os.getenv("OPENAI_API_KEY"),
-    model="gpt-4",
-    temperature=0.1,
-    max_tokens=512,
-    top_p=0.9,
-    top_k=40,
-)
-agent_LLM_Ollama = OllamaModel( # On Private Server
-    url="http://localhost:11434/api/generate",
-    model="llama3",
-    temperature=0.1,
-    max_tokens=512,
-    top_p=0.9,
-    top_k=40,
+# agent_LLM_OpenAI = OpenAIChatCompletionClient( # On OpenAI API
+#     api_key = os.getenv("OPENAI_API_KEY"),
+#     model="gpt-4o",
+#     temperature=0.1,
+#     max_tokens=512,
+#     top_p=0.9,
+#     top_k=40,
+# )
+
+agent_LLM_Ollama = OllamaChatCompletionClient( # On Private Server
+    model="qwen2.5-coder",
+    base_url="http://localhost:11434/v1",  # Adjust the base URL as needed
+    model_info={
+        "vision": False,
+        "function_calling": True,
+        "json_output": True,
+        "family": "unknown",
+        "structured_output": True,
+    },
 )
 
 # Define the summarizer agent using the Ollama model
-summarizer_agents = AssistantAgent(
+summarizer_agent = AssistantAgent(
     name="SummarizerAgent",
     description="An agent that summarizes text.",
     model_client=agent_LLM_Ollama,
@@ -53,9 +102,32 @@ summarizer_agents = AssistantAgent(
 )
 
 # Define the researcher agent using python library ```arxiv```
-researcher_agents = AssistantAgent(
-    name="ResearcherAgent",
-    description="An agent that writes literature review report in Markdown format.",
+arxiv_researcher_agent = AssistantAgent(
+    name="Arxiv_ResearcherAgent",
+    description="An agent that search, retrieves papers from arxiv.com",
     model_client=agent_LLM_Ollama,
-    system_message=system_message,
+    system_message=system_message_arxiv,
+    tools=["arxiv_search"]
 )
+
+# Define the agents list
+# agents = [summarizer_agents, researcher_agents]
+# print(arxiv_search("machine learning", 5)) # Working
+
+team = AgentTeam(
+    name="ArxivResearchTeam",
+    description="A team of agents that search and summarize papers from arxiv.com",
+    agents=[arxiv_researcher_agent, summarizer_agent],
+)
+
+async def team_output():
+    """
+    Run the team and return the output.
+    """
+    async for output in team.run(
+        input="AI Agents",
+        max_iterations=1,
+        max_concurrent_agents=2,
+        max_concurrent_tools=2,
+    ):
+        yield output
